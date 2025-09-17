b6ca74b63b9e8cc85dbed288ca4698f75938f33e
Isabel Valles Bertomeu <IVALBER@upv.edu.es>
2025-09-17 16:11:49 +0200

    preparando argumentos

---
+++ main.py
@@ -0,0 +1,57 @@
+# Require: Una función resumen h y una función recodificante r
+# Require: t longitud de la secuencia
+# Require: n número de entradas
+# Ensure: Una tabla rainbow para la función h.
+
+
+import hashlib
+
+# ----------- PARÁMETROS ----------------
+
+    # ALGORITMO HASH: SHA-256
+    # Nº CARACTERES HASH: 40 bits
+    # Nº CARACTERES CONTRASEÑA: 5 caracteres
+    # FUNCION REDUCCION: 
+
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+PSW_LEN = 5
+HASH_LEN = 40
+TRUNC_LEN = 8
+
+# ----------- PARÁMETROS ----------------
+
+
+
+
+
+# CONSTRUCCION
+
+
+
+
+# BUSQUEDA
+
+
+
+# tabla = tabla vacia
+# while La tabla no contenga n entradas do
+    # Escoger Pi un password al azar.
+    # P = Pi
+    # for j = 1 to t − 1 do
+        # P = r(h(P))
+    # end for
+    # Almacenar ⟨P1, h(P)⟩ en la tabla
+# end while
+
+
+# SHA-256
+# TRUNCAR
+
+# funcion de reconstruccion =
+# dominio = 5 caracteres letras minusculas
+
+# 40 bits en 5 minusculas
+# cada 8 bits saco una letra minuscula
+#  
+# 40 bits de hash
+# el password tiene 5 caracteres
